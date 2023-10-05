0d5e0add0e607a65a45fa08c5495899b095e13a4
Aram_Nayebbandi <devaramnye@gmail.com>
2023-10-05 15:52:42 +0200

    project finish

---
+++ javascript.js
@@ -0,0 +1,17 @@
+const email = document.getElementById("mail");
+const form = document.getElementById("form");
+const emailError = document.getElementById("error");
+
+email.addEventListener("input", (event) => {
+  if (!email.validity.valid) {
+    emailError.textContent = "Please provide a valid email address"
+    } else {
+    emailError.textContent = "";
+  }
+});
+
+form.addEventListener("submit", (event) => {
+  if (!email.validity.valid) {
+    event.preventDefault();
+  }
+});
